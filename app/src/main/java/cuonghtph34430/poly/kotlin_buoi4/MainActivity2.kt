009eae5f3612f0fae27222fc1b232fdda0bf6fbd
cuonghtph34430 <cuonghtph34430@fpt.edu.vn>
2024-05-16 10:24:35 +0700

    Buoi4

---
+++ app/src/main/java/cuonghtph34430/poly/kotlin_buoi4/MainActivity2.kt
@@ -0,0 +1,10 @@
+package cuonghtph34430.poly.kotlin_buoi4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
